7bcfe072f8468e7e127316f402be5228cd1c4e48
Josue <arevalo.josued@gmail.com>
2021-12-03 00:43:18 +0000

    refactored JSON the Cat and Mocha tested

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
 const request = require('request');
-const args = process.argv.slice(2);
 
-const fetcher = function(breed) {
-  request("https://api.thecatapi.com/v1/breeds/search?q=" + breed, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breedName, (error, response, body) => {
     if (error) {
-      console.log('Cat–astrophe! Something went wrong =( \n', error);
+      callback(error, null);
     } else {
       const data = JSON.parse(body);
       if (data.length === 0) {
-        console.log("Error: It's paw-sible the breed is not right!");
+        callback("It's paw-sible the breed is not right!", null);
       } else {
-        console.log("Here's your cat! \n", data[0].description);
+        callback(null, data[0].description);
       }
     }
   });
@@ -18,4 +17,4 @@ const fetcher = function(breed) {
 
 };
 
-fetcher(args[0]);
+module.exports = { fetchBreedDescription };
